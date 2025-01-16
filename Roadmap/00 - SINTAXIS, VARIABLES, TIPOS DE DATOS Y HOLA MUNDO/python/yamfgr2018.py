6d31700ddb92478b27eab34699d5572c313040ac
yamfgr2018 <yamfgr2018@gmail.com>
2025-01-15 19:27:24 -0500

    #00 - PYTHON

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yamfgr2018.py
@@ -0,0 +1,25 @@
+#python.org
+# Comentario en una linea #
+
+"""
+comentario en bloque con 3 comillas dobles, 3 para abrir y cerrar
+"""
+
+'''
+manera de comentario con 3 comillas simples para los comnetario en bloque para abrir y cerrar
+'''
+
+mi_variable = "Mi mundo Python"
+MI_CONSTANTE= "Mi constante"
+mi_numero= 8
+mi_decimal= 32.5
+mi_booleano= True
+mi_booleano_falso= False
+mi_str='mi python'
+
+print("Hola mi lenguaje de programacion en Python")
+print(type(mi_variable))
+print(type(MI_CONSTANTE))
+print(type(mi_numero))
+print(type(mi_decimal))
+print(type(mi_booleano))
