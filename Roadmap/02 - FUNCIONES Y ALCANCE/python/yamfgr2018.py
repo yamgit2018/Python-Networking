b986a984f0e3863fe996d2834da8cb60addf5f86
yamfgr2018 <yamfgr2018@gmail.com>
2025-01-21 19:21:13 -0500

    Avanzado la parte de funcion en Python

---
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/yamfgr2018.py
@@ -0,0 +1,38 @@
+#Funciones definida por el usuario:
+"""def hola():
+    print("Hola Python")
+
+hola()
+
+#Funcion con retorno
+def saludo():
+    return "Hola Yam lets code Python"
+print (saludo())"""
+
+#Funcion con argumento
+#saludo="Hola que tal Any"
+"""def saludo_argumento(nombre, apellido):
+    print(f"Que tal {nombre} {apellido}")
+
+saludo_argumento("Yam","Guerra")"""
+
+"""def funcion_argumento(nombre="Any"):
+    print(f"Hola que tal {nombre}")
+
+funcion_argumento()
+funcion_argumento("Yam")"""
+    
+
+""""def funcion_argumento_dos(saludo, persona):
+    print(f"{saludo} soy {persona}")
+
+funcion_argumento_dos("Hola", "Yam")
+funcion_argumento_dos("Monkey", "Hola")
+funcion_argumento_dos(persona="Any", saludo="Hola")"""
+
+#Argumento y retorno
+def funcion_argu_retorno(nombre, apellido):
+    return f"{nombre} {apellido}"
+
+print(funcion_argu_retorno("Yam", "Guerra"))
+
