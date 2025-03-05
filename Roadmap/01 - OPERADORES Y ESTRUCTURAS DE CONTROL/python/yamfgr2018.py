cad7f8a5fb78b5124bd6a1345b2a67ec6a0b7345
yamfgr2018 <yamfgr2018@gmail.com>
2025-03-05 13:45:00 -0500

    Finalizamos el tema de operadores con un ejercicio final

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/yamfgr2018.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/yamfgr2018.py
@@ -14,7 +14,7 @@ EJERCICIO:
  * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3."""
 ######################################################################################
 #Operadores de comparación
-print(f"Igualdad de 3 = 3: {3==3}")
+"""""print(f"Igualdad de 3 = 3: {3==3}")
 print(f"Desigualdad de 10 != 3: {10!=3}")
 print(f"Mayor de 10 > 3: {10>3}")
 print(f"Menor de 3 < 15: {3<10}")
@@ -53,5 +53,68 @@ entero_1=int(5000)
 entero_2=int(5000)
 print(f"entero_1 is entero_2 {entero_1 is entero_2}")
 print(f"entero_1 is not entero_2 {entero_1 is not entero_2}")
+
+#Operadores de pertenencia 
+print(f"La letra a esta en Yam: {"a" in "Yam"}")
+print(f"La letra z no  esta en Yam: {"z" not in "Yam"}")
+
+#Operadores de bit
+valor1=10 #1010
+valor2=3 #0011
+print(f"AND:10&3: {10 & 3}") #0010
+print(f"OR:10&3: {10 | 3}") #1011
+print(f"XOR:103: {10 ^ 3}") #1001   
+print(f"NOT ~ calc: {~10}")#1010 not 0101
+print(f"Desplazamiento a la derecha {10>>2}")#1010 >> 0010
+print(f"Desplazamiento a la izquierda {10<<2}")#1010 >> 101000"""""
+
+
+#Estructura de Control
+#Condicional
+"""nombreCompleto="Maia Vasquez"
+if nombreCompleto=="Yam Guerra":
+    print(f"El es {nombreCompleto}")
+elif nombreCompleto=="Ana Cespedes":
+    print(f"Ella es {nombreCompleto}")
+else:
+    print(f"Esta persona no es Yam Guerra ni Ana Cespedes, es {nombreCompleto}")"""
+
+#Iterativa
+
+"""for iteracion in range(1,limite,1):
+    if (iteracion%2==0):
+        print(iteracion)"""
+"""limite=50
+iteracion=1
+while(iteracion<=limite):
+    if(iteracion%2==0):
+        print(iteracion)
+    iteracion+=1
+"""
+
+#Excepciones
+"""valor_ingresado=float(input("Ingrese el numero a dividir 10/"))
+try:
+    resultado=10/valor_ingresado
+except ZeroDivisionError:
+     print(f"Error en el servicio")
+else:
+    print (f"Resultado es : {resultado}")
+finally:
+    print("Finalizo el servicio")
+"""
+#Ejercicio 
+ # Crea un programa que imprima por consola todos los números comprendidos
+ # entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+
+valor_numerico_final=55
+for valor_impreso in range(10,valor_numerico_final+1):
+    if((valor_impreso%2==0) and (valor_impreso!=16) and (valor_impreso%3!=0)):
+        print(valor_impreso)
+
+
+
+        
+
 
 
