95ccf37c284ad43dda14e8dabbf2107fbdcd5d18
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-05-07 11:09:14 -0500

    Empezamos aprendiendo las variables basicas en Python

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yamfgr2018.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yamfgr2018.py
@@ -1,25 +1,52 @@
-#python.org
-# Comentario en una linea #
+#Link:
+# www.python.org
+#Comentario en una linea 
 
 """
-comentario en bloque con 3 comillas dobles, 3 para abrir y cerrar
+Comentario de comillas dobles
+Comentario en bloque 
+en varias linea
 """
 
 '''
-manera de comentario con 3 comillas simples para los comnetario en bloque para abrir y cerrar
+Sniffer de Red Básico (Usando Scapy)
+
+Scapy es una potente librería de Python para manipulación y análisis de paquetes de red. Vamos a expandir el sniffer básico para hacerlo más útil en análisis de seguridad.
+
 '''
 
-mi_variable = "Mi mundo Python"
-MI_CONSTANTE= "Mi constante"
-mi_numero= 8
-mi_decimal= 32.5
-mi_booleano= True
-mi_booleano_falso= False
-mi_str='mi python'
 
-print("Hola mi lenguaje de programacion en Python")
-print(type(mi_variable))
-print(type(MI_CONSTANTE))
-print(type(mi_numero))
-print(type(mi_decimal))
-print(type(mi_booleano))
+packet="eth 00:00:00:00:00:00"
+packet="eth 01:01:01:01:01:01:01"
+
+PRIORIDAD_ID= 32768 #Convencion
+PRIORIDAD_ID=4096
+#print (f"Esta es la prioridad: {PRIORIDAD_ID}")
+#print("Esta es mi mac principal:"+  packet)
+#Ptyhon no tiene constantes
+
+#Entero
+cost= 4
+
+#No existe decimales en Networking pero bueno ha practicar Python
+pi=3.14
+
+#Valores Booleanos
+paquete_fragamentado=False
+vlan_tagging=True
+
+
+#Cadena de texto
+trama="Es una trama de capa 2"
+paquete='Es un paquete de capa 3'
+
+mensaje_networking="Los paquetes no mienten"
+print(trama)
+print(paquete)
+print("!!!"+mensaje_networking+'!!!')
+print (f"!!!{mensaje_networking}!!!")
+
+
+
+
+
