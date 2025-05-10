16119f5e2f8312656a563fcc486025ec74b941ba
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-05-10 16:47:43 -0500

    Practicando los operadores en python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/yamfgr2018.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/yamfgr2018.py
@@ -12,109 +12,96 @@ EJERCICIO:
  * DIFICULTAD EXTRA (opcional):
  * Crea un programa que imprima por consola todos los números comprendidos
  * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3."""
-######################################################################################
-#Operadores de comparación
-"""""print(f"Igualdad de 3 = 3: {3==3}")
-print(f"Desigualdad de 10 != 3: {10!=3}")
-print(f"Mayor de 10 > 3: {10>3}")
-print(f"Menor de 3 < 15: {3<10}")
-print(f"Mayor igual de 40 >= 15: {40>=15}")
-print(f"Menor igual de 3 <= 3: {3<=3}")
+#####################################################################################
+import binascii    # Conversiones binarias
+import ipaddress   # Manejo de direcciones IP y redes
+import socket      # Operaciones de red de bajo nivel
+#Operadores Aritmeticos
+"""prioridad_sw1=4096
+prioridad_sw2=32768    
+SW1_IP="192.168.10.100"
+SW2_IP="192.168.11.200"
+print(f"Suma para aumentar la prioridad de SW1 Brigde ID SW1: {prioridad_sw1+4096}")
+print(f"Resta para convertir a SW2 en root bridge Root ID SW2: {prioridad_sw2-4096}" )
 
+print(f"Multiplicacion ICMP de   a 192.168.11.200 x4:")
+for contador_icmp in range(4):
+    print(f"Ping de {SW1_IP} a {SW2_IP}")
+
+print(f"Valor decimal de 8 bits es: {2**8} ")
+print(f"Valores de 8 bits son: 0 a {2**8-1}")
+
+#Operadores de comparacion
+MAC_SW1="aa.04.23.45.56.36"
+MAC_SW2="45.36.89.ff.ab.45"
+if (MAC_SW2==MAC_SW1):
+   print(f"Mac SW1 es igual Mac SW2")
+else:
+    print("No son iguales Mac SW1 con Mac SW2")
+
+if (MAC_SW2!=MAC_SW1):
+     print("No son iguales Mac SW1 con Mac SW2")
+else:
+     print(f"Mac SW1 es igual Mac SW2")    
+
+
+if(prioridad_sw2>prioridad_sw1):
+    print("Prioridad SW2  tiene mayor que SW1")
+else:
+    print("Prioridad SW1  tiene mayor que SW2")
+
+    
 #Operadores Logicos
-print(f"AND &&: 10+3==13 and 5-2==4 es {10+3==13 and 5-2==4}")
-print(f"OR |: 10+3==13 OR 5-2==4 es {10+3==13 or 5-2==4}")
-print(f"NOT !: 5*5==15  es {not (5*5==15)}")
+#AND  TRUE TRUE los dos deben ser true
+#OR   TRUE solo uno debe ser
+#NOT  AL TRUE lo vuelve FALSE y FALSE lo vuelve TRUE
+
+if((MAC_SW1>MAC_SW2) and (prioridad_sw1<prioridad_sw2)):
+    print("TRUE")
+else:
+    print("FALSE")
 
 #Operadores de asignacion
-cell_phone=935375856
-print(f"Mi numero celular es: {cell_phone}")
+SVI_VLAN10="192.168.10.5"
+print(f"IP INT VLAN10= {SVI_VLAN10}")
 
-contador=8 #Asignacion
-print(f"El numero contador es: {contador}")
-contador+=0.5 #Suma
-print(f"El numero contador es: {contador}")
-contador**=0.5 #Exponente
-print(f"El numero contador es: {contador}")
-contador//=0.5 #Division Entera
-print(f"El numero contador es: {contador}")
-contador-=0.5 #Resta
-print(f"El numero contador es: {contador}")
-contador*=0.5 #Multiplicacion
-print(f"El numero contador es: {contador}")
-contador/=0.5 #Division
-print(f"El numero contador es: {contador}")
-contador%=0.5 #Modulo
-print(f"El numero contador es: {contador}")
+loop=0
+while(loop<=10):
+    print(loop)
+    loop+=1
 
+#Operadores de Identidad
+#MAC_SW2=MAC_SW1
+print(f"MAC_SW1 no es igual MAC_SW2: {MAC_SW1 is not MAC_SW2}")
 
-#Operadores de identidadpwd
-entero_1=int(5000)
-entero_2=int(5000)
-print(f"entero_1 is entero_2 {entero_1 is entero_2}")
-print(f"entero_1 is not entero_2 {entero_1 is not entero_2}")
+#Operadores de pertenecia
+print(f"'aa' pertenece a la MAC_SW1 {'aa' in MAC_SW1}")
+print(f"'00' No pertenece a la MAC_SW1 {'00' not  in   MAC_SW1}")
+"""
+#Operadores de Bit
+"""ip = ipaddress.IPv4Address('192.168.10.240')
+ip +=5  
+print(ip)
 
-#Operadores de pertenencia 
-print(f"La letra a esta en Yam: {"a" in "Yam"}")
-print(f"La letra z no  esta en Yam: {"z" not in "Yam"}")
-
-#Operadores de bit
-valor1=10 #1010
-valor2=3 #0011
-print(f"AND:10&3: {10 & 3}") #0010
-print(f"OR:10&3: {10 | 3}") #1011
-print(f"XOR:103: {10 ^ 3}") #1001   
-print(f"NOT ~ calc: {~10}")#1010 not 0101
-print(f"Desplazamiento a la derecha {10>>2}")#1010 >> 0010
-print(f"Desplazamiento a la izquierda {10<<2}")#1010 >> 101000"""""
-
-
-#Estructura de Control
-#Condicional
-"""nombreCompleto="Maia Vasquez"
-if nombreCompleto=="Yam Guerra":
-    print(f"El es {nombreCompleto}")
-elif nombreCompleto=="Ana Cespedes":
-    print(f"Ella es {nombreCompleto}")
-else:
-    print(f"Esta persona no es Yam Guerra ni Ana Cespedes, es {nombreCompleto}")"""
-
-#Iterativa
-
-"""for iteracion in range(1,limite,1):
-    if (iteracion%2==0):
-        print(iteracion)"""
-"""limite=50
-iteracion=1
-while(iteracion<=limite):
-    if(iteracion%2==0):
-        print(iteracion)
-    iteracion+=1
+red=ipaddress.IPv4Network('192.168.10.0/24')
+print(f"Esta es l mascara de red: {red.netmask}")
+print(f"Esta es la ip de broadcast: {red.broadcast_address}")
+print(f"Esta es la ip de red: {red.network_address}")
 """
 
-#Excepciones
-"""valor_ingresado=float(input("Ingrese el numero a dividir 10/"))
-try:
-    resultado=10/valor_ingresado
-except ZeroDivisionError:
-     print(f"Error en el servicio")
-else:
-    print (f"Resultado es : {resultado}")
-finally:
-    print("Finalizo el servicio")
-"""
-#Ejercicio 
- # Crea un programa que imprima por consola todos los números comprendidos
- # entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
-
-valor_numerico_final=55
-for valor_impreso in range(10,valor_numerico_final+1):
-    if((valor_impreso%2==0) and (valor_impreso!=16) and (valor_impreso%3!=0)):
-        print(valor_impreso)
+"""ip=ipaddress.IPv4Address('192.168.10.10')
+ip_int=int(ip)
 
 
-
-        
-
-
-
+#Obtenemos la IP de red:
+mascara=0xFFFFFF00 #/24
+red_resultante=ip_int & mascara
+print(ipaddress.IPv4Address(red_resultante))"""
+valorb1=0b1100
+valorb2=0b1010
+#Operador and
+print(bin(valorb1&valorb2))
+#Operadpr or
+print(bin(valorb1| valorb2))
+#Operador xor
+print(bin(valorb1))
