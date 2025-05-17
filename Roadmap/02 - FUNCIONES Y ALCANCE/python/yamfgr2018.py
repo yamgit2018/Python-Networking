df9919cdacce28ba2384be369d4b8d6c02f3605a
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-05-17 18:50:06 -0500

    Trabajamos con funciones si argumentos y con argumentos

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/yamfgr2018.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/yamfgr2018.py
@@ -16,11 +16,34 @@ print (login)
 print(bannerLogin())"""
 
 #Funcion con argumento
-def  insertIP(ip):
+"""def  insertIP(ip):
     print(f"Esta es la ip ingresada {ipaddress.IPv4Address(ip)}")
 
 ip_nueva=input(f"Ingresa la ip:")
-insertIP(ipaddress.IPv4Address(ip_nueva))
+insertIP(ipaddress.IPv4Address(ip_nueva))"""
+
+#Funcion con mas de un argumento
+def impresion_net_mascara(ip, mascara):
+   interfaz=ipaddress.ip_interface(f"{ip}/{mascara}")
+
+   print(f"Esta es la ip net: {interfaz.ip}")
+   print(f"Esta es la mascara: {interfaz.netmask}")
+   print(f"Esta es direcccion de red:{interfaz.network.network_address}")
+   print(f"Esta la direccion de Broadcast: {interfaz.network.broadcast_address}")
+
+         
+    
+ip=input("Ingresa direccion ip (ejm 192.168.10.0):")
+mascara=input("Ingrese la mascara (ejm 24 / 255.255.255.0):")
+impresion_net_mascara(ip,mascara)
+
+
+
+
+
+
+
+
 
 
 
