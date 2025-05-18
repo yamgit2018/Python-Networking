a063f3f27d7f67e75f00909a10b93cef584c3ff7
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-05-18 15:00:39 -0500

    Hicimos funciones con retorno, con parametro, con parametros,parametros por defecto

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/yamfgr2018.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/yamfgr2018.py
@@ -23,19 +23,56 @@ ip_nueva=input(f"Ingresa la ip:")
 insertIP(ipaddress.IPv4Address(ip_nueva))"""
 
 #Funcion con mas de un argumento
-def impresion_net_mascara(ip, mascara):
+"""def impresion_net_mascara(ip, mascara):
    interfaz=ipaddress.ip_interface(f"{ip}/{mascara}")
 
    print(f"Esta es la ip net: {interfaz.ip}")
    print(f"Esta es la mascara: {interfaz.netmask}")
    print(f"Esta es direcccion de red:{interfaz.network.network_address}")
-   print(f"Esta la direccion de Broadcast: {interfaz.network.broadcast_address}")
+   print(f"Esta la direccion de Broadcast: {interfaz.network.broadcast_address}")"""
+
+    
+"""ip=input("Ingresa direccion ip (ejm 192.168.10.0):")
+mascara=input("Ingrese la mascara (ejm 24 / 255.255.255.0):")
+impresion_net_mascara(ip,mascara)"""
+
+"""def timers_stp(hello_time=2, forward_delay_listening=15,forwar_delay_learning=15,max_age=20):
+   print(f"Hello Time: {hello_time}")
+   print(f"Litening: {forward_delay_listening}")
+   print(f"Learning:{forwar_delay_learning}")
+   print(f"Max Age:{max_age}")"""
+
+
+#timers_stp()
+"""timers_stp(20,30,30,0)
+timers_stp()
+timers_stp(hello_time=5,forwar_delay_learning=5,forward_delay_listening=12)"""
+
+
+#Funcion argumentos y retorno
+
+def subnet_completa(ip,mascara):
+    subnet_1=ipaddress.ip_interface(f"{ip}/{mascara}")
+    return subnet_1
+
+
+ip=input("Ingresa la ip de la red ejm(172.16.10.40): ")
+mascara=input("Ingresa la mascara ejm(24 / 255.255.255.0): ")
+
+red=ipaddress.ip_interface(subnet_completa(ip,mascara))
+print(f"Esta es la ip: {red.ip}")
+print(f"Esta es la mascara: {red.netmask}")
+print(f"Esta es la ip red: {red.network.network_address}")
+print(f"Esta es la ip broadcast:  {red.network.broadcast_address}")
+
+
+
+
+
+
 
          
-    
-ip=input("Ingresa direccion ip (ejm 192.168.10.0):")
-mascara=input("Ingrese la mascara (ejm 24 / 255.255.255.0):")
-impresion_net_mascara(ip,mascara)
+
 
 
 
