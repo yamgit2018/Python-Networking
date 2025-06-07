97591fcf1246883d1fabcd15b96b2f6d2537f097
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-06-07 08:18:32 -0500

    Practicamos el tema ingreso, eliminacion, reemmplazo en un item de la lista

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/yamfgr2018.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/yamfgr2018.py
@@ -14,9 +14,37 @@ numero_subnet=5
 
 subnet=[ipaddress.IPv4Network(f"172.17.{indice}.0/24") for indice in range (0,numero_subnet*hop,hop)]
 print("\n")
+print("Ingresmos una lista de subnets Planta 1")
+for red in subnet:
+    print(red)
+"""
+#append insercion de una dato a la lista:
+subnet.append(ipaddress.IPv4Network("172.17.50.0/24"))
+print("\n")
+print("Ingresamos una subnet 172.17.50.0/24")
 for red in subnet:
     print(red)
 
+# Borrado de un item a la lista
+subnet.remove(ipaddress.IPv4Network("172.17.0.0/24"))
+print("\n")
+print("Eliminamos  una subnet 172.17.0.0/24")
+for red in subnet:
+    print(red)
+"""
+#Actualizacion de la lista subnet
+#Cambiamos la subnet 172.17.50.0 x 172.17.60.0
+subnet_reemplazar=ipaddress.IPv4Network(input("Ingresa la subnet a remplazar:"))
+subnet_nueva=ipaddress.IPv4Network(input("Ingresa la nueva subnet:"))
 
+if subnet_reemplazar in subnet:
+    index=subnet.index(subnet_reemplazar)
+    subnet[index]=subnet_nueva
+    print("\n Subnet ingresada exitosamente")
+else:
+    print("\n Esta subnet no se encuentra en lista")
+    
 
-
+print("\nLista Actualizada")
+for red in subnet:
+    print(red)
