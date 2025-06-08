62f85f096fa0af8016b0c549575c40a0d685f6e7
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-06-08 12:16:01 -0500

    Aprendimos sobre temas de listas y empezamos con tuplas, vamos por mas, dame luz Python

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/yamfgr2018.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/yamfgr2018.py
@@ -1,6 +1,6 @@
 import ipaddress
 #Estructura de Datos:
-#Lista
+#Lista []
 """subred_planta1= ["172.17.0.0/24", "172.17.10.0/24","172.17.20.0/24"]
 
 print(subred_planta1)
@@ -11,14 +11,20 @@ for red in subred_planta1:
 
 hop=10
 numero_subnet=5
-
 subnet=[ipaddress.IPv4Network(f"172.17.{indice}.0/24") for indice in range (0,numero_subnet*hop,hop)]
 print("\n")
+print(subnet)
+print("\n")
+for red in subnet:
+    print(red)
+print(type(subnet))
+print("\n")
+"""print("\n")
 print("Ingresmos una lista de subnets Planta 1")
 for red in subnet:
     print(red)
-"""
-#append insercion de una dato a la lista:
+""""""
+#Insercion de una dato a la lista:
 subnet.append(ipaddress.IPv4Network("172.17.50.0/24"))
 print("\n")
 print("Ingresamos una subnet 172.17.50.0/24")
@@ -34,7 +40,7 @@ for red in subnet:
 """
 #Actualizacion de la lista subnet
 #Cambiamos la subnet 172.17.50.0 x 172.17.60.0
-subnet_reemplazar=ipaddress.IPv4Network(input("Ingresa la subnet a remplazar:"))
+"""subnet_reemplazar=ipaddress.IPv4Network(input("Ingresa la subnet a remplazar:"))
 subnet_nueva=ipaddress.IPv4Network(input("Ingresa la nueva subnet:"))
 
 if subnet_reemplazar in subnet:
@@ -47,4 +53,15 @@ else:
 
 print("\nLista Actualizada")
 for red in subnet:
-    print(red)
+    print(red)"""
+
+#Ordenacion de los datos
+"""subnet.sort(reverse=True)
+print("\n")
+for red  in subnet:
+    print(red)"""
+
+#Tuplas ()
+tupla_subnet=("172.17.0.0/24", "172.17.10.0/24","172.17.20.0/24")
+print(tupla_subnet)
+print(type(tupla_subnet))
