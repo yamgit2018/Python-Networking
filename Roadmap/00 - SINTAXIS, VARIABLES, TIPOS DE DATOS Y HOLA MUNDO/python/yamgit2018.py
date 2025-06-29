5905c9bb80a928d9e7b94f1a2e0aa41a408a5417
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-06-29 07:54:54 -0500

    Empezamos el proyecto python con Networking

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yamgit2018.py
@@ -0,0 +1,9 @@
+import ipaddress
+ip=ipaddress.IPv4Address("10.0.0.240")
+print(f"Esta en la IP de la interface:{ip}")
+print(type(ip))
+
+# Pagina de Python: https://www.python.org/
+"""Pagina de Libreria de pyton ipaddress: https://docs.python.org/3/library/ipaddress.html"""
+
+
