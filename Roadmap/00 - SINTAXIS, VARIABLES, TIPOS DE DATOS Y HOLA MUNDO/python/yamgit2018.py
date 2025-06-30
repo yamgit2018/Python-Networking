f38621d0a7911dd3a976ebcdd49a368ed7012612
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-06-30 17:39:39 -0500

    Practicamos variables tipo constantes

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yamgit2018.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yamgit2018.py
@@ -1,9 +1,30 @@
 import ipaddress
-ip=ipaddress.IPv4Address("10.0.0.240")
-print(f"Esta en la IP de la interface:{ip}")
-print(type(ip))
+"""ip=ipaddress.IPv4Address("10.0.0.240")
+print(f"\nEsta en la IP de la interface {ip}")"""
+#print(type(ip))
 
 # Pagina de Python: https://www.python.org/
 """Pagina de Libreria de pyton ipaddress: https://docs.python.org/3/library/ipaddress.html"""
 
+"""ip_net=ipaddress.IPv4Network("192.168.10.0/30")
+print(f"\nEsta es la red 192.168.10.0/30: {ip_net}")"""
 
+#Constantes
+
+NETMASK_24=ipaddress.IPv4Address("255.255.255.0")
+NETMASK_16=ipaddress.IPv4Address("255.255.0.0")
+NETMASK_8=ipaddress.IPv4Address("255.0.0.0")
+
+def calcular_red(ip,mask):
+    interface=ipaddress.IPv4Interface(f"{ip}/{mask}")
+    return interface.network
+
+ip=input("Inserta una ip y su mascara ejm:192.168.10.5: ")
+mask=input("Ingresa la mascara: ejm:/24 o /16 o /8: ")
+ 
+if (mask == "/24"):
+    print(calcular_red(ip,NETMASK_24))
+elif(mask == "/16"):
+    print(calcular_red(ip,NETMASK_16))
+else:
+    print(calcular_red(ip,NETMASK_8))
