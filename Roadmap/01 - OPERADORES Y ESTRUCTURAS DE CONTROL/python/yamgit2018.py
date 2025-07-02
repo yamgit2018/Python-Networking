103e20f4ee354a487ed78e7de15399df686de760
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-07-02 17:28:59 -0500

    Empezamos hoy con operadores a divertirnos

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/yamgit2018.py
@@ -0,0 +1,19 @@
+import ipaddress
+#Operadores
+#IPv4
+ip=ipaddress.IPv4Address("192.168.10.100")
+print (f"Esta es la ip: {ip}")
+print (f"Suma +5: {ip+5}")
+print (f"Resta -10: {ip-10}")
+
+#IPv6
+ipv6=ipaddress.IPv6Address("2001:db8::a")
+print (f"\nEsta es la IPv6: {ipv6}")
+print (f"Sumamnos +10: {ipv6+10}")
+print (f"Restamos -10: {ipv6-10}")
+
+pool_ip=ipaddress.IPv4Address("10.0.1.100")
+print("\n Agregamos 5 ips del pool 10.0.1.100")
+for ip in  range (1,10):
+    print (f"IP {ip} - {pool_ip+ip}")
+
